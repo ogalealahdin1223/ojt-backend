3c115a37827921a88ed90b8f048faf3477dc10d7
Alahdin Ogale <ogalealahdin1223@gmail.com>
2026-03-24 23:28:14 +0800

    fix mysql

--- ojt_backend/ojt_backend/settings.py
+++ ojt_backend/ojt_backend/settings.py
@@ -96,13 +96,16 @@ ASGI_APPLICATION = 'ojt_backend.asgi.application'
 # DATABASE
 # --------------------------------------------------
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.environ.get('DB_NAME', ''),
-        'USER': os.environ.get('DB_USER', ''),
-        'PASSWORD': os.environ.get('DB_PASSWORD', ''),
-        'HOST': os.environ.get('DB_HOST', ''),
-        'PORT': os.environ.get('DB_PORT', ''),
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": os.getenv("DB_NAME"),
+        "USER": os.getenv("DB_USER"),
+        "PASSWORD": os.getenv("DB_PASSWORD"),
+        "HOST": os.getenv("DB_HOST"),
+        "PORT": os.getenv("DB_PORT"),
+        "OPTIONS": {
+            "init_command": "SET sql_mode='STRICT_TRANS_TABLES'",
+        },
     }
 }
 
